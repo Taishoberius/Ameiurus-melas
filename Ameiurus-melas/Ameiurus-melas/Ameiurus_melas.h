05280d369e341c321776574e324a2030e49ed713
David Linhares <linhares.david@digipolitan.com>
2019-01-16 18:11:03 +0100

    :tada: Add cocoa framework project / Update README.md

---
+++ Ameiurus-melas/Ameiurus-melas/Ameiurus_melas.h
@@ -0,0 +1,19 @@
+//
+//  Ameiurus_melas.h
+//  Ameiurus-melas
+//
+//  Created by David Linhares on 16/01/2019.
+//  Copyright © 2019 David Linhares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ameiurus_melas.
+FOUNDATION_EXPORT double Ameiurus_melasVersionNumber;
+
+//! Project version string for Ameiurus_melas.
+FOUNDATION_EXPORT const unsigned char Ameiurus_melasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ameiurus_melas/PublicHeader.h>
+
+
