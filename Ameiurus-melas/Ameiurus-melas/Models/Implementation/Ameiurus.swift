a53ec6eaa469520370a168e1fd3d4e1bd0f0eeac
David Linhares <linhares.david@digipolitan.com>
2019-02-23 15:47:45 +0100

    :sparkles: Add ameiurus object for controlling conversation

---
+++ Ameiurus-melas/Ameiurus-melas/Models/Implementation/Ameiurus.swift
@@ -0,0 +1,9 @@
+//
+//  Ameiurus.swift
+//  Ameiurus-melas
+//
+//  Created by David Linhares on 23/02/2019.
+//  Copyright © 2019 David Linhares. All rights reserved.
+//
+
+import Foundation
---
+++ Ameiurus-melas/Ameiurus-melas/Models/Implementation/ApiType.swift
@@ -0,0 +1,9 @@
+//
+//  ApiType.swift
+//  Ameiurus-melas
+//
+//  Created by David Linhares on 23/02/2019.
+//  Copyright © 2019 David Linhares. All rights reserved.
+//
+
+import Foundation
