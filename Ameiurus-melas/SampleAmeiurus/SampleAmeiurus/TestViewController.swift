18c97eaa5ea99f0facb00d345d93607e128d6d0b
Cédric Nanteau <cedric.nanteau@gmail.com>
2019-02-23 15:48:32 +0100

    [WIP] pour que david teste

---
+++ Ameiurus-melas/SampleAmeiurus/SampleAmeiurus/TestViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TestViewController.swift
+//  SampleAmeiurus
+//
+//  Created by Cédric NANTEAU Dev on 07/02/2019.
+//  Copyright © 2019 Cedric NANTEAU Dev. All rights reserved.
+//
+
+import UIKit
+import Ameiurus_melas
+
+class TestViewController: UIViewController {
+
+    @IBAction func testButton(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Ameiurus-melas/SampleAmeiurus/SampleAmeiurus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleAmeiurus
+//
+//  Created by Cédric NANTEAU Dev on 07/02/2019.
+//  Copyright © 2019 Cedric NANTEAU Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
